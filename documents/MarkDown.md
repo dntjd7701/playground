b6d1d61b6318fb68282ca120af0ffcdedc897192
강우성 <peacegrace7701@douzone.com>
2024-06-04 22:54:45 +0900

    delete all

---
+++ documents/MarkDown.md
@@ -0,0 +1,1 @@
+### "react-markdown": "^9.0.1"
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
-import Markdown from 'react-markdown'
-import README from '/README.md'
+import Markdown from 'react-markdown';
+import README from '/README.md';
+import MarkDown from '../../documents/MarkDown.md';
 
 const MainPage = () => (
-  <div className="markdown">
+  <div className='markdown'>
+    {/* <Markdown>{README}</Markdown> */}
     <Markdown>{README}</Markdown>
   </div>
-)
+);
 
-export default MainPage
+export default MainPage;
