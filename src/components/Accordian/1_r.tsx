831c0707f09dd1fe64fff1ecaaa6d65065b3ff30
강우성 <peacegrace7701@douzone.com>
2024-06-26 23:00:43 +0900

    Accordion 시작

---
+++ src/components/Accordian/1_r.tsx
@@ -0,0 +1,4 @@
+const Accordion1 = () => {
+  return <div>아코디언 1</div>;
+};
+export default Accordion1;
--- src/components/Accordian/index.tsx
+++ src/components/Accordian/index.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import cx from '@/components/Accordian/cx';
+import Accordion1 from '@/components/Accordian/1_r';
 
 const Accordian = () => {
-  return <h1>Accordian</h1>;
+  return (
+    <div className={cx('Accordions')}>
+      <h2>아코디언</h2>
+      <Accordion1 />
+    </div>
+  );
 };
 
 export default Accordian;
